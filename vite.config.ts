6effe4a4e28c985d20fd665ac192d9df0b2b2753
YASH B KOPARDE <yashkoparde2022@gmail.com>
2025-12-21 16:46:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '')
 
   return {
     base: '/hawkins-hack-registration/',
+
     plugins: [react()],
 
     server: {
